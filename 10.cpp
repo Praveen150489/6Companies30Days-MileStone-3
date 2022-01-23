534501ccdcdfc37d1447a8d46a1458a37fdfee65
PRAVEEN PRAKASH <73226469+Praveen150489@users.noreply.github.com>
2022-01-23 19:33:25 +0530

    Add files via upload

---
+++ 10.cpp
@@ -0,0 +1,18 @@
+// Stickler Thief .
+
+class Solution
+{
+    public:
+    //Function to find the maximum money the thief can get.
+    int FindMaxSum(int arr[], int n)
+    {
+        // Your code here
+        int t[n+1];
+        t[0]=0;
+        t[1]=arr[0];
+        for(int i=2;i<=n;i++){
+            t[i]=max(arr[i-1]+t[i-2],t[i-1]);
+        }
+        return t[n];
+    }
+};
